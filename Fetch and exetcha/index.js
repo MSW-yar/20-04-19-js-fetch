249e9d9b93691673a699aba1540dde7ec79d3b3f
MSW-yar <apnashery96@yahoo.com>
2019-04-20 20:08:24 +0500

    adding fetch from json placeholder

---
+++ Fetch and exetcha/index.js
@@ -0,0 +1,7 @@
+ fetch("http://192.168.1.106:3000/taha")
+ .then(response =>response.json())
+ .then (json => console.log(json));
+ //that was buit in function
+
+ // let express = require ('express');
+ // let a = []
